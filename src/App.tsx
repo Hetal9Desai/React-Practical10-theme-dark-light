7668a6bc5ac0a5f4750ef0d8d8f0a0389e2bc1f9
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-21 14:28:48 +0530

    Add: Add Practical 5 TODO App code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import { TaskListWrapper } from './components/Task/TaskListWrapper';
+import { AddEditTaskWrapper } from './components/Task/AddEditTaskWrapper';
+import NotFound from './components/NotFound/NotFound';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <main className="container mt-4">
+        <Routes>
+          <Route path="/" element={<TaskListWrapper />} />
+          <Route path="/add-task" element={<AddEditTaskWrapper />} />
+          <Route path="/edit-task/:taskId" element={<AddEditTaskWrapper />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
